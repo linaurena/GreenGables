c9fd4ff0e491601145ba3497413f6c821f9d0bdb
linaurena <lina8maria8@hotmail.com>
2021-07-22 18:38:36 -0500

    Cover title and media queries fixed

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,28 @@
-import'./styles/Header.css'
+import './styles/Header.css';
+import TitleImg from './img/name.png';
+import BookIcon from './img/book.png';
+import QuillIcon from './img/quill.png';
 
 function Header () {
-    let movement = window.scrollY
-    console.log(movement)
-    return (
-      <header>
-          <div className='cover'>
-          </div>
-        <nav>
-            <p>Green Gables</p>
-        </nav>
-      </header>
-    )
-  }
+  let movement = window.scrollY
+  console.log(movement)
+  return (
+    <header>
+        <div className='cover'>
+          <img src={TitleImg} alt='Anne with an E'/>
+        </div>
+      <nav>
+          <ul>
+            <li className='link-nav'>
+              <a href='./'><img src={BookIcon} alt='Book icon' className='nav-icons'/> Docs</a>
+            </li>
+            <li className='link-nav'>
+              <a href='./'><img src={QuillIcon} alt='Quill icon' className='nav-icons'/>Contact</a>
+            </li>
+          </ul>
+      </nav>
+    </header>
+  )
+}
 
 export default Header;
